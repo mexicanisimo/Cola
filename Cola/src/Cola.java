d9fb41eb1ca3d4c03b0f9c1f01d1f45a0c3f3a0d
mexicanisimo <mexicanisimo1200@gmail.com>
2017-02-16 23:47:44 -0600

    Es una modificacion

--- Cola/src/Cola.java
+++ Cola/src/Cola.java
@@ -9,83 +9,79 @@ public class Cola {// comportamiento encapsulado
     private int frente; //frente es el final
     private int fondo;//inicio
     private int size;
-    private  int c[]=new int [5];
-    private  int s[]=new int[10];
+    private int c[] = new int[5];
+    private int s[] = new int[10];
+
     // Inserta --> Extrae  definen a la cola
     // encapsulada  --> recorrer --> buscar -->size
-    public Cola(int  cantidad){
+    public Cola(int cantidad) {
 
-        this.frente=0;
-        this.fondo=0;
-        this.size=cantidad;
+        this.frente = 0;
+        this.fondo = 0;
+        this.size = cantidad;
 
-        }
+    }
 
-    public void insertar(int valor){
-        if(fondo <size){
-           // System.out.println("eL SIZE "+size + "EL FONDO" + fondo);
+    public void insertar(int valor) {
+        if (fondo < size) {
+            // System.out.println("eL SIZE "+size + "EL FONDO" + fondo);
             System.out.print("->" + valor);
-            c[fondo]=valor;
+            c[fondo] = valor;
             fondo++;
-        }else{
-            JOptionPane.showMessageDialog(null,"Cola llena papu ");
+        } else {
+            JOptionPane.showMessageDialog(null, "Cola llena papu ");
         }
     }
 
-    public int extraer(){
-        int tem=-1;
-        int elimina=0;
-      //  System.out.println("frente"+ frente);
-        System.out.println("Final de cola "+ fondo);
-        if(frente==fondo){
-            JOptionPane.showMessageDialog(null,"La cola esta vacia puedes insertar desde un principio");
-            fondo=0;
-            frente=0;
-        }else{
+    public int extraer() {
+        int tem = -1;
+        int elimina = 0;
+        //  System.out.println("frente"+ frente);
+        System.out.println("Final de cola " + fondo);
+        if (frente == fondo) {
+            JOptionPane.showMessageDialog(null, "La cola esta vacia puedes insertar desde un principio");
+            fondo = 0;
+            frente = 0;
+        } else {
             System.out.println("El frente  es : " + c[frente]);
-            c[frente]=elimina;
-            tem=c[frente];
+            c[frente] = elimina;
+            tem = c[frente];
             frente++;
         }
         return tem;
     }
 
-    public void recorrer(){
+    public void recorrer() {
 
-        for( int i=frente;i<fondo;i++){
+        for (int i = frente; i < fondo; i++) {
             System.out.print(c[i] + "->");
         }
     }
-    public boolean Buscar(int valor){
-        boolean b=false;
 
-        int i=0;
-        while(i<size&& c[i]!=valor)
+    public boolean Buscar(int valor) {
+        boolean b = false;
+
+        int i = 0;
+        while (i < size && c[i] != valor)
             i++;
-        if(i<size){
-            b=true;
+        if (i < size) {
+            b = true;
         }
         return b;
     }
-    public  boolean Llena(){
-        if(this.frente==this.fondo){
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    public int size(){
-        return size;
-    }
 
-    public boolean lol() {
+    public boolean Llena() {
         if (this.frente == this.fondo) {
             return true;
         } else {
             return false;
         }
     }
+
+    public int size() {
+        return size;
+    }
+
 }
 
 
